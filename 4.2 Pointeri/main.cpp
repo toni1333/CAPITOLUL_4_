fe80c130d250968680427e7aa120c1cb2a510c74
toni1333 <antoniebohak@yahoo.com>
2020-07-31 21:29:50 +0300

    `

--- 4.2 Pointeri/main.cpp
+++ 4.2 Pointeri/main.cpp
@@ -5,16 +5,14 @@ int main()
 {
     short ident1, ident2, identSuma, identProdus;
     short *pntident1, *pntident2, *pntidentSuma, *pntidentProdus;
-
-	pntident1= &ident1;
-    pntident2= &ident2;
-	pntidentSuma = &identSuma;
-    pntidentProdus = &identProdus;
-	*pntident1= 1000;
-    *pntident2= 2;
-	*pntidentSuma= (*pntident1 + *pntident2);
-    *pntidentProdus= (*pntident1 * *pntident2);
-
+     pntident1= &ident1;
+     pntident2= &ident2;
+	 pntidentSuma = &identSuma;
+	 pntidentProdus = &identProdus;
+	 *pntident1= 1000;
+	 *pntident2= 2;
+	 *pntidentSuma= (*pntident1 + *pntident2);
+	 *pntidentProdus= (*pntident1 * *pntident2);
 	cout <<"Suma variabilelor este:      "<<*pntidentSuma<<endl;
 	cout <<"Produsul variabilelor este:  "<<*pntidentProdus<<endl;
 
